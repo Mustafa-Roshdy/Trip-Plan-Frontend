647094ad677ffe06b051823f708405193f0e93fc
Mustafa Mohamed Roshdy <120448988+Mustafa-Roshdy@users.noreply.github.com>
2025-12-03 10:52:58 +0200

    backend ip

--- src/interceptor/api.ts
+++ src/interceptor/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://trip-plan-backend-two.vercel.app/",
 });
 
 // Request interceptor
